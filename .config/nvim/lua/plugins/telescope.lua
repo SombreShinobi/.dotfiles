2ceac20f5295af76c7f1c55283c1de5c06e78d00
SombreShinobi <sedlan.sibin@gmail.com>
2024-02-27 18:08:03 +0100

    feat: simplify telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -12,7 +12,6 @@ return {
 
             pcall(telescope.load_extension, "fzf")
 
-            vim.keymap.set("n", "<leader>?", builtin.oldfiles)
             vim.keymap.set("n", "<leader><space>", builtin.buffers)
             vim.keymap.set("n", "<leader>/", function()
                 builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
@@ -22,7 +21,7 @@ return {
             end)
 
             vim.keymap.set("n", "<leader>sf", builtin.find_files)
-            vim.keymap.set("n", "<leader>sh", builtin.help_tags)
+            vim.keymap.set("n", "<leader>st", builtin.help_tags)
             vim.keymap.set("n", "<leader>sw", builtin.grep_string)
             vim.keymap.set("n", "<leader>sg", builtin.live_grep)
             vim.keymap.set("n", "<leader>sh", function()
