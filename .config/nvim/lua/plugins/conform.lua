9fb06e6b213764070782e88e439a7e138dc29ad0
SombreShinobi <sedlan.sibin@gmail.com>
2024-08-15 11:33:54 +0200

    fix: conform deprecated config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,19 +2,20 @@ return {
 	"stevearc/conform.nvim",
 	opts = {
 		async = true,
+		timeout_ms = 500,
 		formatters_by_ft = {
 			lua = { "stylua" },
-			javascript = { { "prettierd", "prettier" } },
-			typescript = { { "prettierd", "prettier" } },
-			javascriptreact = { { "prettierd", "prettier" } },
-			typescriptreact = { { "prettierd", "prettier" } },
-			json = { { "prettierd", "prettier" } },
-			go = { { "gofumpt" } },
-			templ = { { "templ" } },
+			javascript = { "prettierd", "prettier" },
+			typescript = { "prettierd", "prettier" },
+			javascriptreact = { "prettierd", "prettier" },
+			typescriptreact = { "prettierd", "prettier" },
+			json = { "prettierd", "prettier" },
+			go = { "gofumpt" },
+			templ = { "templ" },
 		},
 		format_on_save = {
 			timeout_ms = 500,
-			lsp_fallback = true,
+			stop_after_first = true,
 		},
 	},
 }
