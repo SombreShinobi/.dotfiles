9987851a7623d29323f9016b94d3bcf10d33bcae
SombreShinobi <sedlan.sibin@gmail.com>
2026-05-13 14:08:09 +0200

    feat: update treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,47 +1,52 @@
+local langs = {
+	"c",
+	"javascript",
+	"jsdoc",
+	"json",
+	"html",
+	"css",
+	"scss",
+	"yaml",
+	"lua",
+	"rust",
+	"zig",
+	"typescript",
+	"tsx",
+	"dockerfile",
+	"ocaml",
+	"go",
+	"python",
+	"swift",
+	"java",
+	"sql",
+	"markdown",
+	"markdown_inline",
+}
+
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
+		branch = "main",
 		event = "VeryLazy",
+		build = ":TSUpdate",
 		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
+			{
+				"nvim-treesitter/nvim-treesitter-textobjects",
+				branch = "main",
+			},
 		},
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				sync_install = false,
-				ignore_install = {},
-				auto_install = true,
-				modules = {},
-				ensure_installed = {
-					"c",
-					"javascript",
-					"jsdoc",
-					"json",
-					"html",
-					"css",
-					"scss",
-					"yaml",
-					"lua",
-					"rust",
-					"zig",
-					"typescript",
-					"tsx",
-					"dockerfile",
-					"ocaml",
-					"go",
-					"python",
-					"swift",
-					"java",
-					"sql",
-				},
-				highlight = {
-					enable = true,
-				},
-				indent = {
-					enable = true,
-					disable = { "python" },
-				},
+			require("nvim-treesitter").setup()
+
+			require("nvim-treesitter").install(langs)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = langs,
+				callback = function(args)
+					pcall(vim.treesitter.start, args.buf)
+				end,
 			})
+
 			vim.filetype.add({
 				extension = {
 					templ = "templ",
